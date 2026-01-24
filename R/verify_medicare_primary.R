81ded849b5a11a43efde57f51d0573b9247c18f1
Vagish Hemmige <vahemmig@montefiore.org>
2026-01-23 19:33:20 -0500

    Add ability to pass coverage df to function

--- R/verify_medicare_primary.R
+++ R/verify_medicare_primary.R
@@ -4,6 +4,9 @@
 #' the name of a date variable in quotes, and a variable `lookback_days` that encodes how many days of Medicare
 #' coverage are necessary.
 #'
+#' An optional variable `medicare_coverage_df` allows for a df containing information from the `payhist` file to be passed to the function.  If left
+#' NULL, then the function will load the data via the `load_usrds_file` function.
+#'
 #' It returns the same dataframe with a new variable `medicare_primary_TF` that takes the values `TRUE`
 #' or `FALSE`.
 #'
@@ -13,6 +16,8 @@
 #' @param index_date either a fixed date or the name of a date variable in quotes
 #'
 #' @param lookback_days is the number of days of Medicare as primary necessary.  Default value is 365.
+#'
+#' @param medicare_coverage_df is an optional parameter containing a df from the `payhist` file.
 #'
 #' @return The original `df` data fame with the variable `medicare_primary_TF` added
 #'
@@ -30,7 +35,7 @@
 #'
 #' }
 
-verify_medicare_primary<-function(df, index_date, lookback_days=365)
+verify_medicare_primary<-function(df, index_date, lookback_days=365, medicare_coverage_df=NULL)
 {
 
   #Error checking
@@ -40,12 +45,22 @@ verify_medicare_primary<-function(df, index_date, lookback_days=365)
     rlang::abort("`df` must be a data frame.")
   }
 
+  #Check that medicare_coverage_df is an appropriate data frame
+  if (!inherits(medicare_coverage_df, "data.frame") && !is.null(medicare_coverage_df)) {
+    rlang::abort("`medicare_coverage_df` must be a data frame or NULL.")
+  }
+
   if (nrow(df) == 0) {
     rlang::abort("`df` must have at least one row.")
   }
 
   if (!"USRDS_ID" %in% names(df)) {
     rlang::abort("`df` must contain a column named `USRDS_ID`.")
+  }
+
+  if (!is.null(medicare_coverage_df) &&
+      !"USRDS_ID" %in% names(medicare_coverage_df)) {
+    rlang::abort("`medicare_coverage_df` must contain a column named `USRDS_ID`.")
   }
 
   if (all(is.na(df$USRDS_ID))) {
@@ -90,11 +105,23 @@ verify_medicare_primary<-function(df, index_date, lookback_days=365)
     }
   }
 
+  #Warning if df containus USRDS_IDs that are not in the medicare_coverage_df (assuming the latter is not NULL)
+  if (!is.null(medicare_coverage_df)) {
+
+    missing_ids <- setdiff(df$USRDS_ID, medicare_coverage_df$USRDS_ID)
+
+    if (length(missing_ids) > 0) {
+      rlang::warn(glue::glue(
+        "{length(missing_ids)} USRDS_ID(s) in `df` are not present in `medicare_coverage_df`."
+      ))
+    }
+  }
+
 #--------------------------------------------------------------------------------------------
 
   # Now we begin the code which executes the purpose of the function
 
-  #Convert index_date to something that can be used
+  #Convert index_date if necessary to something that can be used
   if (is.character(index_date) && length(index_date) == 1) {
     index_date_var <- rlang::sym(index_date)
     df <- df %>% mutate(.index_date = !!index_date_var)
@@ -110,8 +137,16 @@ eligible_payers <- c(
 )
 
 #Open Medicare history file, keep rows where the payer is Medicare primary, and sort by USRDS and date
-medicare_history<-load_usrds_file("payhist",
-                                  usrds_ids = df$USRDS_ID)%>%
+
+if (is.null(medicare_coverage_df)) {
+  medicare_history<-load_usrds_file("payhist",
+                                    usrds_ids = df$USRDS_ID)
+} else
+{
+  medicare_history<-medicare_coverage_df
+}
+
+medicare_history<-medicare_history%>%
   filter(PAYER %in% eligible_payers) %>%
   select(USRDS_ID, BEGDATE, ENDDATE)%>%
   arrange(USRDS_ID, BEGDATE)%>%
@@ -143,10 +178,11 @@ patients_clean<-left_join(df,
                           join_by(USRDS_ID, between(.index_date, BEGDATE, ENDDATE))
 )
 
-#Filter by FFS coverage on day of claim through 365 days prior
+#Create variable that marks FFS coverage on day of claim through 365 days prior
+#Logic here depends strongly on nature of between join in dplyr
 patients_clean<-patients_clean%>%
   mutate(medicare_primary_TF=ifelse(!is.na(BEGDATE) & .index_date-BEGDATE>=lookback_days,TRUE,FALSE))%>%
-  select(-.index_date)
+  select(-.index_date, -BEGDATE, -ENDDATE)
 
 return(patients_clean)
 
